2f666d71ac1194efb1637a833f91064081d93267
Admin <admin@adon.network>
2019-05-20 19:30:09 +0530

    Bug fixes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // settings
-const api = "http://127.0.0.1:19901";
+const api = "https://blocks.adon.network/explore";
 const coinDifficultyTarget = 120;
 const symbol = "ADON";
 const coinUnits = 100000000;
@@ -8,10 +8,10 @@ const transactionExplorer = "?hash={id}#transaction";
 
 // poolList ["id","visible", "url", "api", "enabled"]
 const poolList = [
-    { "id": 1, "visible": true, "url": "https://pool.adon.network", "api": "https://pool.adon.network:8117", "status": true, "flag": "US", "loc": "US" }
+    { "id": 1, "visible": true, "url": "https://pool.adon.network", "api": "https://pool.adon.network/api", "status": true, "flag": "US", "loc": "US" },
+    { "id": 2, "visible": true, "url": "https://adon.cryptonote.club", "api": "https://adon.cryptonote.club:8199", "status": true, "flag": "DE", "loc": "DE" }
 ];
 const seedsNodes = [
     { "id": "rbx", "url": "http://01.seed.adon.network:19901" },
-    { "id": "mlt", "url": "http://01.seed.adon.network:19901" },
-    // {"id":"sdn","url":"https://ns543550.ip-139-99-131.net"},
+    { "id": "mlt", "url": "http://01.seed.adon.network:19901" }
 ];
